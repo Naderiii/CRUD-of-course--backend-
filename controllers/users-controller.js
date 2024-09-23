12bb509f9ec5c398c8d383b9ae883b12f6a0e361
hediyeh naderi <naderi.h.72@gmail.com>
2024-09-24 01:04:19 +0330

    fix bug of create token

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -2,14 +2,13 @@ const UsersModel = require("../models/users-model");
 const Joi = require("joi");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 //-------------------------- register ---------------------------
 
 const register = async (req, res, next) => {
-
   const schema = {
-    username: Joi.string().min(3).max(50).required().messages({"string.min": "Minimum characters required"}),
+    username: Joi.string().min(3).max(50).required().messages({ "string.min": "Minimum characters required" }),
     email: Joi.string().email().required(),
     password: Joi.string().min(5).max(50).required(),
   };
@@ -28,24 +27,25 @@ const register = async (req, res, next) => {
     req.body.email,
     hashPassword
   );
-  console.log(result);
 
+  // Fetch the new user after registration
   const newUser = await UsersModel.getUserByEmail(req.body.email);
 
-  const token =jwt.sign({ id: user.id }, "your_secret_key_100%_secret");
+  // Use newUser to generate the JWT token
+  const token = jwt.sign({ id: newUser.id }, "your_secret_key_100%_secret");
 
-  res.header("Authorization", token).send(_.pick(newUser, ["id", "username", "email"]));   //it dosen't show password
+  // Respond with the token and user info (excluding the password)
+  res.header("Authorization", token).send(_.pick(newUser, ["id", "username", "email"]));
 };
 
 //--------------------------------- login ---------------------------
 
 const login = async (req, res, next) => {
-
   const schema = {
     email: Joi.string().email().required(),
     password: Joi.string().min(5).max(50).required(),
   };
-  
+
   const validateResult = Joi.object(schema).validate(req.body);
   if (validateResult.error)
     return res.send(validateResult.error.details[0].message);
@@ -56,9 +56,10 @@ const login = async (req, res, next) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword)
     return res.status(400).send("email or password is invalid");
-  const token =jwt.sign({ id: user.id }, "your_secret_key_100%_secret");
+
+  // Generate JWT token upon successful login
+  const token = jwt.sign({ id: user.id }, "your_secret_key_100%_secret");
   res.send(token);
 };
-
 
 module.exports = { register, login };
