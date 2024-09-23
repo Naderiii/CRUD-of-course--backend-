dcdefe8b091cfccfdccf95d212fb893732a2ca6b
hediyeh naderi <naderi.h.72@gmail.com>
2024-09-23 18:48:02 +0330

    fix bug of courses controller

--- controllers/courses-controller.js
+++ controllers/courses-controller.js
@@ -1,52 +1,54 @@
-// const { result } = require('underscore');
-const CoursesModel = require('../models/courses-model-mysql')
-
+const CoursesModel = require('../models/courses-model-mysql');
 
 const getCourse = (req, res) => {
-  console.log();
-  CoursesModel.getCourse(parseInt(req.params.id)).then((result) => {
-    if (!result) res.status(404).send("course with given id not found");
-    res.send(result);
-  });
+  CoursesModel.getCourse(parseInt(req.params.id))
+    .then((result) => {
+      if (!result) return res.status(404).send("Course with the given ID not found");
+      res.send(result);
+    })
+    .catch((err) => res.status(500).send("Server error"));
 };
 
 const getCourses = (req, res) => {
-  CoursesModel.getCourses().then((result) => {
-    res.send(result);
-  });
+  CoursesModel.getCourses()
+    .then((result) => res.send(result))
+    .catch((err) => res.status(500).send("Server error"));
 };
 
 const insertCourse = (req, res) => {
-  console.log(req.body);
   if (!req.body.name || req.body.name.length < 3) {
-    res.status(400).send("name is required");
-    return;
+    return res.status(400).send("Name is required and must be at least 3 characters long");
   }
-  CoursesModel.insertCourse(req.body.name).then((result) => res.send(result));
+  
+  CoursesModel.insertCourse(req.body.name)
+    .then((result) => res.send(result))
+    .catch((err) => res.status(500).send("Server error"));
 };
 
 const updateCourse = (req, res) => {
-  CoursesModel.getCourse(parseInt(req.params.id)).then((result) => {
-    if (!result) return res.status(404).send("course with given id not found");
-  });
+  CoursesModel.getCourse(parseInt(req.params.id))
+    .then((course) => {
+      if (!course) return res.status(404).send("Course with the given ID not found");
+      
+      if (!req.body.name || req.body.name.length < 3) {
+        return res.status(400).send("Name is required and must be at least 3 characters long");
+      }
 
-  if (!req.body.name || req.body.name.length < 3)
-    return res.status(400).send("name is required and more than 3 charachter");
-
-  CoursesModel.updateCourse(parseInt(req.params.id), req.body.name).then(
-    (result) => {
-      res.send(result);
-    }
-  );
+      return CoursesModel.updateCourse(parseInt(req.params.id), req.body.name);
+    })
+    .then((updatedCourse) => res.send(updatedCourse))
+    .catch((err) => res.status(500).send("Server error"));
 };
 
 const deleteCourse = (req, res) => {
-  CoursesModel.getCourse(parseInt(req.params.id)).then((result) => {
-    if (!result) return res.status(404).send("course with given id not found");
-  });
-  CoursesModel.deleteCourse(parseInt(req.params.id)).then((result) => {
-    res.send(result);
-  });
+  CoursesModel.getCourse(parseInt(req.params.id))
+    .then((course) => {
+      if (!course) return res.status(404).send("Course with the given ID not found");
+
+      return CoursesModel.deleteCourse(parseInt(req.params.id));
+    })
+    .then(() => res.send(`Course with ID ${req.params.id} deleted`))
+    .catch((err) => res.status(500).send("Server error"));
 };
 
 module.exports = {
