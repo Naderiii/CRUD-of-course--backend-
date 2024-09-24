38a79f0cce711bf989e8131cea08449e4c5e07bb
hediyeh naderi <naderi.h.72@gmail.com>
2024-09-24 04:19:16 +0330

    Merge pull request #9 from Naderiii/7-authentication-user
    7 authentication user

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,5 +1,18 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
 const authentication = ((req, res, next) => {
-    console.log("authentication");
-    next();
+    
+    const token = req.header("Authorization")
+    if(!token) return res.status(401).send("Access denied")
+
+    try{
+    const decode = jwt.verify(token , process.env.SECRET_KEY)
+    req.userData = decode
+    next()
+    }
+    catch(er){
+        return res.status(400).send("token is invalid")
+    }
 })
 module.exports = authentication;
--- routers/courses-routes.js
+++ routers/courses-routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const coursesController = require("../controllers/courses-controller");
+const auth = require("../middlewares/authentication");
+
+// router.use(auth);  // to protect the routes with JWT token.  // auth middleware is required for all routes under /api/courses
 
 router.get("/:id", coursesController.getCourse);
 
-router.get("/", coursesController.getCourses);
+router.get("/", auth, coursesController.getCourses);
 
 router.post("/", coursesController.insertCourse);
 
