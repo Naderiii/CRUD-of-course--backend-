d99402d9b23038b54bbf35a0100e20f399eb1811
hediyeh naderi <naderi.h.72@gmail.com>
2024-09-24 22:56:41 +0330

    add logger to error handler

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,7 +1,17 @@
 const AppError = require("../utilities/app_error");
+const {createLogger, format, transports} = require("winston")
+
+
+const logger = createLogger({
+  format : format.json(),
+  transports : [
+    new transports.Console({level : "info"})
+  ]
+})
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error);
+  // console.log(error);
+  logger.log("info", "this is message for winston");
   if (error.name === "ValidationError")
     return res.status(500).send("validation is failed");
 
