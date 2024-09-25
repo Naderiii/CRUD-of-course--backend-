0ae17af531bf931524ca098693548ed072ddf7ba
hediyeh naderi <naderi.h.72@gmail.com>
2024-09-25 16:53:55 +0330

    complete logging error

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,22 +1,10 @@
 const AppError = require("../utilities/app_error");
-const { createLogger, format, transports } = require("winston");
-
-const logger = createLogger({
-  format: format.combine(
-    format.timestamp(),
-    format.json()
-  ),
-  transports: [
-    new transports.Console({ level: "info" }),
-    new transports.File({ filename: "winstonLogger.log", level: "error" }),
-  ],
-});
+const logger = require("../utilities/winstonLogger");
 
 const errorHandler = (error, req, res, next) => {
   // Logging error details for debugging
   logger.log("error", {
     message: error.message,
-    stack: error.stack,
     name: error.name,
     url: req.originalUrl,
     method: req.method,
--- utilities/winstonLogger.js
+++ utilities/winstonLogger.js
@@ -3,15 +3,13 @@ const { createLogger, format, transports } = require("winston");
 const logger = createLogger({
   format: format.combine(
     format.timestamp(),
-    format.label({ label: "this is lable" }),
-    format.prettyPrint()
+    format.json(),
+    //format.prettyPrint()
   ),
   transports: [
     new transports.Console({ level: "info" }),
-    new transports.File(
-      { filename: "winstonLogger.log", level: "error" },
-      new transports.Http({ level: "warn", host: "localhost", port: 5500 })
-    ),
+    new transports.File({ filename: "winstonLogger.log", level: "error" }),
+    //new transports.Http({level: "warn", host: "localhost", port: 5500, path: "/api/errors"  }),
   ],
 });
 
