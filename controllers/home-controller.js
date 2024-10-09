2de0609381e71d033297f7e9f16058614d8335f3
hediyeh naderi <naderi.h.72@gmail.com>
2024-10-09 18:05:53 +0330

    modify

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,11 +1,11 @@
 const getHome =(req, res) => {
   //  dbDebug("db debug")
-    res.send("hello from sarvinstyle codding");
+    res.send("hello from Hediyeh Naderi");
   
   }
 const getHomePost =(req, res) => {
     console.log(req.body)
-    res.send("hello from sarvinstyle codding");
+    res.send("hello from Hediyeh Naderi");
   
   }
 module.exports = {getHome , getHomePost}
